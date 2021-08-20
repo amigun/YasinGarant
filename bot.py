99e22411c28df82bfb65d7f41d8f3e59d97cb7a6
amigun <amigun.tube@gmail.com>
2021-08-20 17:13:02 +0500

    Move logfiles and database.db to resources

--- bot.py
+++ bot.py
@@ -10,10 +10,10 @@ from loguru import logger as log
 import sqlite3
 
 # Enabled logging
-log.add('logfiles/logging.log', format='{time} {message}', rotation='1 week', compression='zip')
+log.add('resources/logfiles/logging.log', format='{time} {message}', rotation='1 week', compression='zip')
 log.info('Launching the bot. Importing libraries and creating a log file')
 
 # Enabled DB
-db = sqlite3.connect('database.db', check_same_thread=False)
+db = sqlite3.connect('resources/database.db', check_same_thread=False)
 sql = db.cursor()
 log.info('Connecting to the database')
