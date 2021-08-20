e3f7b91bea65887da15d46fbd99583315a1ae0b1
amigun <amigun.tube@gmail.com>
2021-08-20 17:01:19 +0500

    Enable logging and DB

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
-print('Hello World!')
+# Import libs
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher, FSMContext
+from aiogram.utils import executor
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.callback_data import CallbackData
+from loguru import logger as log
+import sqlite3
+
+# Enabled logging
+log.add('logfiles/logging.log', format='{time} {message}', rotation='1 week', compression='zip')
+log.info('Launching the bot. Importing libraries and creating a log file')
+
+# Enabled DB
+db = sqlite3.connect('database.db', check_same_thread=False)
+sql = db.cursor()
+log.info('Connecting to the database')
