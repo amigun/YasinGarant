5dfc83173507fb50b4b131118d3e6cc717d83508
amigun <amigun.tube@gmail.com>
2021-08-21 17:50:22 +0500

    Add init DB

--- database_queries.py
+++ database_queries.py
@@ -1,8 +1,14 @@
+# Enabled DB
+db = sqlite3.connect('resources/database.db', check_same_thread=False)
+sql = db.cursor()
+log.info('Connecting to the DB')
+
 def user_reg(**kwargs):
+    user_id = list(kwargs.items())[0][1]
     sql.execute(f"SELECT id_user FROM users WHERE id_user = '{user_id}'")
 
     if sql.fetchone() is None:
         sql.execute("INSERT INTO users VALUES (id)", (user_id))
         db.commit()
 
-        log.info(f'User {user_id} was registered')
+        log.info(f'User {user_id} was registred')
