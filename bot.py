3b09d1b72ef87488ac21523ea6b46c6b6f75c49e
amigun <amigun.tube@gmail.com>
2021-08-21 19:23:13 +0500

    Add reaction on help command

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ import config, messages
 
 # Enabled logging
 log.add('resources/logfiles/logging.log', format='{time} {message}', rotation='1 week', compression='zip')
-log.info('Launching the bot. Importing libraries and creating a log file')
+log.info('Launching the bot. Importing libraries, connect to DB and creating a log file')
 
 # Init bot
 bot = Bot(token=config.TOKEN)
@@ -20,10 +20,16 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 
 @dp.message_handler(commands=['start'], state='*')
 async def start(msg: types.Message):
-    print(f'\nText: {messages.get("greeting", first_name=msg.from_user.first_name)}')
     await bot.send_message(msg.from_user.id, messages.get('greeting', first_name=msg.from_user.first_name))
 
-    dq.user_reg(user_id=msg.from_user.id)
+    start = dq.user_reg(user_id=msg.from_user.id)
+
+    if start == 1:
+        log.info(f'User {msg.from_user.id} was registred')
+
+@dp.message_handler(commands=['help'], state='*')
+async def help(msg: types.Message):
+    await bot.send_message(msg.from_user.id, messages.get('help'))
 
 if __name__ == '__main__':
     executor.start_polling(dp)
