473fff870c57400c8043de8fcec0bed550c95ef6
amigun <amigun.tube@gmail.com>
2021-08-21 18:20:46 +0500

    Update querie

--- database_queries.py
+++ database_queries.py
@@ -9,7 +9,7 @@ def user_reg(**kwargs):
     sql.execute(f"SELECT id FROM users WHERE id = {user_id}")
 
     if sql.fetchone() is None:
-        sql.execute("INSERT INTO users (id) VALUES (?)", (user_id))
+        sql.execute("INSERT INTO users (id) VALUES (?)", (user_id,))
         db.commit()
 
         return 1
