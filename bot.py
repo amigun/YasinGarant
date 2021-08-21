aa513a0104f86399eef87df59013dfab28865616
amigun <amigun.tube@gmail.com>
2021-08-21 17:07:55 +0500

    Add /start handler

--- bot.py
+++ bot.py
@@ -7,7 +7,9 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.callback_data import CallbackData
 from loguru import logger as log
+import database_queries as dq
 import sqlite3
+import config, messages
 
 # Enabled logging
 log.add('resources/logfiles/logging.log', format='{time} {message}', rotation='1 week', compression='zip')
@@ -16,4 +18,14 @@ log.info('Launching the bot. Importing libraries and creating a log file')
 # Enabled DB
 db = sqlite3.connect('resources/database.db', check_same_thread=False)
 sql = db.cursor()
-log.info('Connecting to the database')
+log.info('Connecting to the DB')
+
+# Init bot
+bot = Bot(token=config.TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+@dp.message_handler(commands=['start'], state='*')
+async def start(msg: types.Message):
+    await bot.send_message(msg.from_user.id, messages.greeting)
+
+    dq.user_reg(user_id=msg.from_user.id)
